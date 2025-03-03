8f3e6575323c379ba17ece6efaeb266c98bcc7e0
John Verdone <jmvedone@gmail.com>
2025-03-03 04:51:43 -0500

    typos

--- app/controllers/api/v1/authentication_controller.rb
+++ app/controllers/api/v1/authentication_controller.rb
@@ -28,15 +28,17 @@ module Api
       end
 
       def login
-        puts "------------"
-        puts permitted_params[:email]
-        User.find_by(email: permitted_params[:email])
 
-        puts "------------"
         @user = User.find_by(email: permitted_params[:email])
 
         if @user&.authenticate(permitted_params[:password])
           token = JwtService.encode(user_id: @user.id.to_s)
+
+          puts "------------"
+          puts permitted_params[:email]
+          User.find_by(email: permitted_params[:email]).email
+
+          puts "------------"
           render json: { token: token, user: { id: @user.id.to_s, name: @user.name, email: @user.email } }
         else
           render json: { error: 'Invalid credentials' }, status: :unauthorized
