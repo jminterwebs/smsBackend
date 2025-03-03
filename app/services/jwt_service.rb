bdfefe6c778edbf959ef5b51c1d32418c26df2cb
John Verdone <jmvedone@gmail.com>
2025-03-03 09:42:56 -0500

    fix auth

--- app/services/jwt_service.rb
+++ app/services/jwt_service.rb
@@ -1,6 +1,6 @@
 # app/services/jwt_service.rb
 class JwtService
-  SECRET_KEY = Rails.application.credentials.secret_key_base
+  SECRET_KEY =  Rails.application.credentials.secret_key_base || ENV['SECRET_KEY_BASE']
 
   def self.encode(payload, exp = 24.hours.from_now)
     payload[:exp] = exp.to_i
